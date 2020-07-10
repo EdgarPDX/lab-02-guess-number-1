dda56fdc3fbd798abc0553a2555f201a0ff446ca
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-07-09 17:05:47 -0700

    added notes to guess-utils and made some css adjustments

--- guess-utils.js
+++ guess-utils.js
@@ -1,10 +1,12 @@
+
+//function compares user guess to correctNumber and returns 0/-1/1
 export function compareNumbers(guess, correctNumber){
     if (guess === correctNumber){
         return 0;
 
-    }else if(guess > correctNumber){
+    } else if (guess > correctNumber){
         return 1;
-    }else{
+    } else {
         return -1;
     }
 }
